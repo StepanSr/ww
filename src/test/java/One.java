7983866017614f636d2cddf9f32ad2b64ae1ec45
StepanS <minme4u@ukr.net>
2019-02-22 12:42:19 -0600

    new

---
+++ src/test/java/One.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class One {
+
+
+
+
+        WebDriver driver;
+
+        @BeforeClass
+        public void setup(){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+
+        @Test
+        public void automationFrame() throws InterruptedException {
+            driver.get("http://automationpractice.com");
+            Thread.sleep(3000);
+            WebElement facebookFrame = driver.findElement(By.xpath("//div[@class='facebook-fanbox']//iframe"));
+            driver.switchTo().frame(facebookFrame);
+            driver.switchTo().defaultContent();
+            WebElement ele = driver.findElement(By.id("facebook_block"));
+            System.out.println(ele.getText());
+            //driver.switchTo().defaultContent();
+            System.out.println(driver.getTitle());
+        }
+    }
+
+
+
+
+
